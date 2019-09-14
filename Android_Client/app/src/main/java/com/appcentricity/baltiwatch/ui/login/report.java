da0570eded79cfca8f18c36c3b1ddafacf42d78a
Christopher Xu <Christopher.xu@live.com>
2019-09-14 10:33:01 -0400

    Changed descriptions, added image to nav drawer

--- Android_Client/app/src/main/java/com/appcentricity/baltiwatch/ui/login/report.java
+++ Android_Client/app/src/main/java/com/appcentricity/baltiwatch/ui/login/report.java
@@ -69,8 +69,8 @@ public class report extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_report, R.id.nav_neighborhood, R.id.nav_rewards,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_report, R.id.nav_neighborhood, R.id.nav_verify,
+                R.id.nav_rewards, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
