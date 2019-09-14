f3028265ece3c1335fa533570ac6b178dd848f03
Christopher Xu <Christopher.xu@live.com>
2019-09-14 10:08:59 -0400

    added ability to write reports to firestore

--- Android_Client/app/src/main/java/com/appcentricity/baltiwatch/ui/login/report.java
+++ Android_Client/app/src/main/java/com/appcentricity/baltiwatch/ui/login/report.java
@@ -1,19 +1,31 @@
 package com.appcentricity.baltiwatch.ui.login;
 
+import android.graphics.Color;
+import android.location.LocationManager;
+import android.media.Image;
 import android.os.Bundle;
 
 import com.appcentricity.baltiwatch.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -21,11 +33,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.w3c.dom.Document;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class report extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    FirebaseFirestore db;
+    DatabaseReference myRef;
+    LocationManager locationManager;
+    boolean trashVal = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +56,7 @@ public class report extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,13 +69,16 @@ public class report extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_report, R.id.nav_neighborhood, R.id.nav_rewards,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        trashVal = false;
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -66,5 +93,49 @@ public class report extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    public void test(View view) {
+
+        if (trashVal) {
+            addReport("trash");
+        } else {
+            Toast selectToast = Toast.makeText(this, "need to select  a type", Toast.LENGTH_LONG);
+            selectToast.show();
+
+        }
+
+
+
+    }
+
+    private void addReport(String type) {
+        Map<String, Object> Report = new HashMap<>();
+        Report.put("Type", type);
+        Report.put("Location", new GeoPoint(1.0, 1.0));
+        DocumentReference ref = db.collection("Reports").document();
+        db.collection("Reports")
+                .add(Report)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("something", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("something", "Error adding document", e);
+                    }
+                });
+    }
+
+    public void toggleTrash(View view) {
+        if (trashVal) {
+            view.setBackgroundColor(Color.LTGRAY);
+        } else {
+            view.setBackgroundColor(Color.GREEN);
+        }
+        trashVal = !trashVal;
+
     }
 }
